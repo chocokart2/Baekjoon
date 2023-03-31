7c628faf13c268243f5018db688dca47c2085267
chocokart2 <chocokart@naver.com>
2023-03-31 09:38:35 +0900

    new 10818, 8958

---
+++ CSharp/10818/try1/try1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace try1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            
+            foreach(string one in Console.ReadLine().Split(' '))
+            {
+                int num = int.Parse(one);
+                if (num < min) min = num;
+                if (num > max) max = num;
+            }
+
+            Console.WriteLine($"{min} {max}");
+        }
+    }
+}
---
+++ CSharp/8958/try1/try1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace try1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            int[] results = new int[count];
+            for(int line = 0; line < count; ++line)
+            {
+                string receiveLine = Console.ReadLine();
+                int streak = 1;
+                for(int index = 0; index < receiveLine.Length; ++index)
+                {
+                    if (receiveLine[index] == 'O')
+                    {
+                        results[line] += streak;
+                        streak++;
+                    }
+                    else
+                    {
+                        streak = 1;
+                    }
+                }
+            }
+            foreach (int one in results)
+            {
+                Console.WriteLine(one);
+            }
+        }
+    }
+}
