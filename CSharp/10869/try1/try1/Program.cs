6f576dc608485f78a8b0136b797d3b043a05daa0
chocokart2 <chocokart@naver.com>
2023-04-05 11:54:02 +0900

    10950번 해결

---
+++ CSharp/10869/try1/try1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace try1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] receiveLine = Console.ReadLine().Split(' ');
+            int numberA = int.Parse(receiveLine[0]);
+            int numberB = int.Parse(receiveLine[1]);
+            Console.WriteLine(numberA + numberB);
+            Console.WriteLine(numberA - numberB);
+            Console.WriteLine(numberA * numberB);
+            Console.WriteLine(numberA / numberB);
+            Console.WriteLine(numberA % numberB);
+        }
+    }
+}
---
+++ CSharp/10950/no10950try1/no10950try1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace no10950try1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            for(int i = 0; i < count; ++i)
+            {
+                string[] stringNum = Console.ReadLine().Split(' ');
+                Console.WriteLine(int.Parse(stringNum[0]) + int.Parse(stringNum[1]));
+            }
+        }
+    }
+}
