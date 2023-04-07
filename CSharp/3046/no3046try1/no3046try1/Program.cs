be2dcc4e8f482245db21f158e98559d5cce4be42
chocokart2 <chocokart@naver.com>
2023-04-07 09:16:30 +0900

    3046번 해결

---
+++ CSharp/3046/no3046try1/no3046try1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace no3046try1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] receiveStrings = Console.ReadLine().Split(' ');
+            Console.WriteLine(int.Parse(receiveStrings[1]) * 2 - int.Parse(receiveStrings[0]));
+        }
+    }
+}
