5a8bc784b70098af32b4822517b16f28eb3cbee0
chocokart2 <chocokart@naver.com>
2023-04-10 08:54:25 +0900

    10952번

---
+++ CSharp/10952/no10952try1/no10952try1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace no10952try1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                string[] nums = Console.ReadLine().Split(' ');
+                if (nums[0].Equals("0")) break;
+                Console.WriteLine(int.Parse(nums[0]) + int.Parse(nums[1]));
+            }
+        }
+    }
+}
