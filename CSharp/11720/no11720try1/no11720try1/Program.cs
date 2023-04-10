38c5645dc84c76d1303fc9a842ba96e57363e4be
chocokart2 <chocokart@naver.com>
2023-04-10 14:19:40 +0900

    11720번 해결

---
+++ CSharp/11720/no11720try1/no11720try1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace no11720try1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            string numbers = Console.ReadLine();
+            int result = 0;
+
+            foreach(char one in numbers)
+            {
+                result += int.Parse(one.ToString());
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
