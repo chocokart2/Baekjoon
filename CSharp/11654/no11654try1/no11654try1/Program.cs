621501fb32435ded3a4652661785bf1d47c8ad1d
chocokart2 <chocokart@naver.com>
2023-04-10 09:01:36 +0900

    11654번 문제 해결

---
+++ CSharp/11654/no11654try1/no11654try1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace no11654try1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine((int)Console.ReadLine()[0]);
+        }
+    }
+}
