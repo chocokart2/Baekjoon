03104e5ede63df7203eb10c59c08a4b6aad40e8c
chocokart2 <chocokart@naver.com>
2023-04-12 08:53:06 +0900

    1001번 해결

---
+++ CSharp/1001/no1001try1/no1001try1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace no1001try1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nums = Console.ReadLine().Split(' ');
+            Console.WriteLine(int.Parse(nums[0]) - int.Parse(nums[1]));
+        }
+    }
+}
---
+++ CSharp/4378/no4378try1/no4378try1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace no4378try1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
